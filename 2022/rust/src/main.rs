d4558c83e3d602181ff4be7f2721ff0af940eec2
Wifu <62569962+Wifus@users.noreply.github.com>
2023-08-19 20:38:36 -0700

    2022: Rust Workflow Streamlining

--- 2022/rust/src/main.rs
+++ 2022/rust/src/main.rs
@@ -1,9 +1,11 @@
 pub mod day1;
 
-const DAY: u8 = 1;
+fn load_input(day: u8) -> String {
+    std::fs::read_to_string(format!("../input/{}.txt", day)).expect(&format!("No input for Day {} found!", day))
+}
 
 fn main() {
-    let input = std::fs::read_to_string(format!("../input/{}.txt", DAY)).expect("Input not found!");
+    let input = load_input(1);
 
     dbg!(day1::part_one(input.clone()));
     dbg!(day1::part_two(input.clone()));
